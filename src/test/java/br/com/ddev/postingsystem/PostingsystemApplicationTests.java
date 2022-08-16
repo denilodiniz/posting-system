928601b5af233509bf974ac9800633e9b9be875d
Denilo Diniz <denilo_diniz@outlook.com>
2022-08-15 23:40:10 -0300

    Project created

---
+++ src/test/java/br/com/ddev/postingsystem/PostingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ddev.postingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
